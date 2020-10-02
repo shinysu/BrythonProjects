0da2d565cfe0c53c088cd3ad70bbc3becd8962d5
shinysu <shiny.suresh@gmail.com>
2020-10-02 15:07:15 +0530

    refactored for better readability

--- todo.py
+++ todo.py
@@ -2,14 +2,15 @@ from browser import document, html
 
 todolist = []
 
+def display_container():
+    container = html.DIV(id="container")
+    container <= display_title()
+    container <= form_div()
+    container <= list_div()
+    return container
+
 def display_title():
     return html.LABEL("Todo List App", id="title")
-
-def display_form():
-    input_item = html.INPUT(type="text", name="item", value="", placeholder="Enter the todo item", id="item")
-    add_btn = html.BUTTON("Add", id="addbtn")
-    add_btn.bind("click", action)
-    return input_item + html.BR() + add_btn
 
 def form_div():
     formdiv = html.DIV(id="formdiv")
@@ -19,7 +20,13 @@ def form_div():
 def list_div():
     return html.DIV(Class="todo-list", id="todo-list")
 
-def delete(event):
+def display_form():
+    input_item = html.INPUT(type="text", name="item", value="", placeholder="Enter the todo item", id="item")
+    add_btn = html.BUTTON("Add", id="addbtn")
+    add_btn.bind("click", add_item)
+    return input_item + html.BR() + add_btn
+
+def delete_item(event):
     item = event.target.id
     todolist.remove(item)
     update_list()
@@ -27,17 +34,17 @@ def delete(event):
 def create_line(item):
     item_name = html.LABEL(item,id="itemtext")
     del_button = html.BUTTON("X", id=item)
-    del_button.bind("click", delete)
+    del_button.bind("click", delete_item)
     return item_name + del_button
 
 def update_list():
-    listdiv.clear()
+    document["todo-list"].clear()
     list_item = (html.LI(create_line(item), Class="todo") for item in todolist)
     print(list_item)
     list = html.UL(list_item)
-    listdiv <= list
+    document["todo-list"] <= list
 
-def action(event):
+def add_item(event):
     item_name = document["item"]
     item = item_name.value
     if item:
@@ -46,11 +53,4 @@ def action(event):
     update_list()
 
 
-container = html.DIV(id="container")
-innerdiv = html.DIV(id="innerdiv")
-innerdiv <= display_title()
-innerdiv <= form_div()
-listdiv = list_div()
-innerdiv <= listdiv
-container <= innerdiv
-document <= container
+document <= display_container()
